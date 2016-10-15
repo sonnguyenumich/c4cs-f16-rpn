e133d61f371d57442a61f8b7df052204b2f88d0a
son nguyen <sxnguyen@umich.edu>
2016-10-15 15:06:57 -0400

    update test_rpn.py

--- test_rpn.py
+++ test_rpn.py
@@ -4,8 +4,8 @@ import rpn
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
-        result = rpn.calculate("1 1 +")
-        self.assertEqual(2, result)
+        result = rpn.calculate("13 1 +")
+        self.assertEqual(14, result)
     def test_subtract(self):
         result = rpn.calculate("101 3 -")
         self.assertEqual(98, result)
